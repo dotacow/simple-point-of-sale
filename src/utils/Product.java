535c25fae364d2d889d11e2204570edaa99d21d9
Yousef Al-shishani <yousef.shaher@gmail.com>
2025-06-05 23:04:28 +0300

    -created addProduct() function

--- src/utils/Product.java
+++ src/utils/Product.java
@@ -4,10 +4,41 @@
  */
 package utils;
 
+import java.io.*;
+import java.sql.*;
 /**
  *
  * @author dotac
  */
 public class Product {
     
+    public enum e_category {
+        PHARMACY,
+        FOOD,
+        HYGIENE
+    }
+    
+    public void addProduct(int ProductId, String name, int quantity, e_category category, File image, double price) {
+        String sql = "INSERT INTO `product`(`ProductId`, `Name`, `StockQuantity`, `Category`, `Image`, `Price`) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (Connection conn = DBHelper.connect();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             FileInputStream fis = new FileInputStream(image)) {
+
+            stmt.setInt(1, quantity);
+            stmt.setString(2, name);
+            stmt.setInt(3, quantity);
+            stmt.setString(4, category.toString());
+            stmt.setBinaryStream(5, fis, (int) image.length());
+            stmt.setDouble(6, price);
+
+            int rows = stmt.executeUpdate();
+            if (rows > 0) {
+                System.out.println("Product inserted successfully.");
+            }
+
+        } catch (SQLException | IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
